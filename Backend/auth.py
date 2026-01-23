4c3ccbf1e0755f6cf27b14ab3a5fb3f492e99cbd
Syed Muhammad Rizvi <143601576+Syedd18@users.noreply.github.com>
2026-01-24 03:13:24 +0530

    Update auth.py

--- Backend/auth.py
+++ Backend/auth.py
@@ -194,7 +194,8 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+    # Bcrypt has a 72 byte maximum password length
+    return pwd_context.hash(password[:72])
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
